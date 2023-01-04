fa724fcc66c0b25cb54fb7236b4a8815cfe02cbc
Hari Tjahjo <haritjahjo@gmail.com>
2023-01-04 20:36:24 +0700

    update migration to add role_id to users table

--- database/migrations/2023_01_03_162510_add_role_id_to_users_table.php
+++ database/migrations/2023_01_03_162510_add_role_id_to_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->foreignId('role_id')->nullable()->default(1)->after('password')->constrained();
         });
     }
 
@@ -26,7 +26,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->foreignId('role_id')->nullable()->default(1)->after('password')->constrained();
+            //
         });
     }
 };
